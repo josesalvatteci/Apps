1786917dc140c48c923d400924db78f89f2ffaa2
The-Financial-Fox <46638505+The-Financial-Fox@users.noreply.github.com>
2025-03-10 19:26:54 +0100

    Update Finance-GPT.py

--- Finance/World-Champ/Finance-GPT.py
+++ Finance/World-Champ/Finance-GPT.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from fbprophet import Prophet
 import os
 from dotenv import load_dotenv
